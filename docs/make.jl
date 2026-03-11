8e111ac68ab90fc06078c544815268745f7c883a
C. Naaktgeboren <NaaktgeborenC.PhD@gmail.com>
2026-03-11 18:35:24 -0300

    UPD the docs/make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -1,17 +1,20 @@
+push!(LOAD_PATH, "../src/")
+
 using ThermoAmounts
 using Documenter
 
-DocMeta.setdocmeta!(ThermoAmounts, :DocTestSetup, :(using ThermoAmounts); recursive=true)
+DocMeta.setdocmeta!(ThermoAmounts, :DocTestSetup, :(using ThermoAmounts); recursive = true)
 
 makedocs(;
-    modules=[ThermoAmounts],
-    authors="C. Naaktgeboren",
-    sitename="ThermoAmounts.jl",
-    format=Documenter.HTML(;
-        edit_link="main",
-        assets=String[],
+    remotes = nothing,
+    modules = [ThermoAmounts],
+    authors = "C. Naaktgeboren",
+    sitename = "ThermoAmounts.jl",
+    format = Documenter.HTML(;
+        edit_link = "main",
+        assets = String[],
     ),
-    pages=[
+    pages = [
         "Home" => "index.md",
     ],
 )
