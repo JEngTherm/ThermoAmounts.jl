3c6e71ee6e5100764a281de2c305006049ff9b1e
C. Naaktgeboren <NaaktgeborenC.PhD@gmail.com>
2026-03-11 18:54:10 -0300

    UPD the src/abstract.jl

--- src/abstract.jl
+++ src/abstract.jl
@@ -2,10 +2,12 @@
 `function tyArchy(t::Union{DataType,UnionAll})`\n
 Returns a string suitable for documenting the hierarchy of an abstract type.
 """
-function tyArchy(t::Union{DataType,UnionAll})
-    h = Any[t]; while h[end] != Any; append!(h, [supertype(h[end])]); end
+function tyArchy(t::Union{DataType, UnionAll})
+    h = Any[t]; while h[end] != Any
+        append!(h, [supertype(h[end])])
+    end
     H = Tuple(string(nameof(i)) for i in h)
-    join(H, " <: ")
+    return join(H, " <: ")
 end
 
 """
@@ -14,36 +16,51 @@ Declares exactly one new, non-parametric, abstract type `TY <: TP`. Argument `wh
 in the new type documentation, and `xp` controls whether or not the new abstract type is
 exported (default `true`).
 """
-function mkNonPAbs(TY::Symbol, TP::Symbol, what::AbstractString, xp::Bool=true)
+function mkNonPAbs(TY::Symbol, TP::Symbol, what::AbstractString, xp::Bool = true)
     if !(eval(TP) isa DataType)
         error("Type parent must be a DataType. Got $(string(TP)).")
     end
     hiStr = tyArchy(eval(TP))
     dcStr = """
-`abstract type $(TY) <: $(TP) end`\n
-Abstract supertype for $(what).\n
-## Hierarchy\n
-`$(TY) <: $(hiStr)`
-    """
-    @eval begin
+    `abstract type $(TY) <: $(TP) end`\n
+    Abstract supertype for $(what).\n
+    ## Hierarchy\n
+    `$(TY) <: $(hiStr)`
+        """
+    return @eval begin
         # Abstract type definition
         abstract type $TY <: $TP end
         # Type documentation
         @doc $dcStr $TY
         # Type exporting
-        if $(xp); export $TY; end
+        if $(xp)
+            export $TY
+        end
     end
 end
 
 # ThermoAmounts root abstract type
-mkNonPAbs(:AbstractTherm   , :Any          , "thermodynamic entities"                         )
+mkNonPAbs(:AbstractTherm, :Any, "thermodynamic entities")
 
 # BASE branch
-mkNonPAbs(  :BASES         , :AbstractTherm, "thermodynamic quantity bases"                   )
-mkNonPAbs(    :IntBase     , :BASES        , "intensive bases"                                )
-mkNonPAbs(      :MA        , :IntBase      , "the MAss base"                                  )
-mkNonPAbs(      :MO        , :IntBase      , "the MOlar base"                                 )
-mkNonPAbs(    :ExtBase     , :BASES        , "non-intensive bases"                            )
-mkNonPAbs(      :SY        , :ExtBase      , "the SYstem (extensive) base"                    )
-mkNonPAbs(      :DT        , :ExtBase      , "the Time Derivative (rate) base"                )
+mkNonPAbs(:BASES, :AbstractTherm, "thermodynamic quantity bases")
+mkNonPAbs(:IntBase, :BASES, "intensive bases")
+mkNonPAbs(:MA, :IntBase, "the MAss base")
+mkNonPAbs(:MO, :IntBase, "the MOlar base")
+mkNonPAbs(:ExtBase, :BASES, "non-intensive bases")
+mkNonPAbs(:SY, :ExtBase, "the SYstem (extensive) base")
+mkNonPAbs(:DT, :ExtBase, "the Time Derivative (rate) base")
 
+"""
+`const PREC = Base.IEEEFloat`\n
+Concrete precision type union for parametric abstract types.
+"""
+const PREC = Base.IEEEFloat
+
+"""
+`const BASE = Union{MA,MO,SY,DT}`\n
+Concrete base type union for parametric abstract types.
+"""
+const BASE = Union{MA, MO, SY, DT}
+
+export PREC, BASE
